0a90718ad6493e49754fdd15392a5bfc0c256f11
Daniel <Dani2912mol@gmail.com>
2021-09-14 13:55:02 -0400

    Agregar archivos de proyecto.

---
+++ ejemplo01/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	std::cout << " Apellidos y Nombres Del Estudiante : Orias Aarncibia Carlos Daniel  " << std::endl;
+	std::cout << " Carrera : Ingenieria De Sistemas " << std::endl;
+	std::cout << " C.U : 35-4799 " << std::endl;
+	return 0;
+}
